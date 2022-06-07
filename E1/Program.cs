1a5ab3c7b01fb43ac72db290a446903562f631a1
Santiago Peraza <santiagoperaza@icloud.com>
2022-06-07 14:28:54 -0300

    Ejercicios OpenBootcamp

---
+++ E1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine(nombre);
---
+++ E2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+
+Console.WriteLine("La hora actual es:" +datetime);
